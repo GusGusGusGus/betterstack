8771a7cfe387f67597c278e9669efd43c741856b
Speedy Guzz <19626457+GusGusGusGus@users.noreply.github.com>
2024-11-18 12:50:55 +0000

    added further XSS validation

--- create.php
+++ create.php
@@ -5,10 +5,10 @@ ini_set('display_errors', 1);
 
 $app = require "./core/app.php";
 
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_SPECIAL_CHARS);
-$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_SPECIAL_CHARS);
+$city = filter_input(INPUT_POST, 'city', FILTER_SANITIZE_STRING);
+$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
 
 if (!$name || !$email || !$city || !$phone) {
     header('Content-Type: application/json');
@@ -26,5 +26,10 @@ $user->insert(array(
 ));
 
 header('Content-Type: application/json');
-echo json_encode(['name' => $name, 'email' => $email, 'city' => $city, 'phone' => $phone]);
+echo json_encode([
+    'name' => htmlspecialchars($name, ENT_QUOTES, 'UTF-8'),
+    'email' => htmlspecialchars($email, ENT_QUOTES, 'UTF-8'),
+    'city' => htmlspecialchars($city, ENT_QUOTES, 'UTF-8'),
+    'phone' => htmlspecialchars($phone, ENT_QUOTES, 'UTF-8')
+]);
 exit;
